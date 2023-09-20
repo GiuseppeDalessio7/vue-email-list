0c2c0192d2b068e3d0088e2f6d6e62b6d600eaa9
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-09-20 15:48:05 +0200

    Finish Exe
    add for  and v-for

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,42 +5,32 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
 const { createApp } = Vue
-  createApp({
+createApp({
     data() {
-      return {
-        generateMail: "",
-      }
+        return {
+            generateMail: [],
+        }
     },
 
-  mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //promessa
-    .then(response =>{
-        console.log(response);
-        console.log(response.data.response);
-        this.generateMail = response.data.response
-        
-    })
-  },
+    mounted() {
+        //ciclo con il for per fare dieci volte la stessa operazione
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //promessa
+                .then(response => {
+                    console.log(response);
+                    console.log(response.data.response);
+                    this.generateMail.push( response.data.response) 
+                })
+        }
+    },
+    methods: {
 
-  methods: {
+    }
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 
-  }
- 
- 
- 
- 
- 
 
-  
-  }).mount('#app')
+
+
+
+
+}).mount('#app')
