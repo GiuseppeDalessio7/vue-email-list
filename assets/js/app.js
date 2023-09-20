912a3d10269eb2b1f37f4d41e6d05430c9325eb6
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-09-20 16:52:36 +0200

    Finish commit
    Add bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,20 +9,32 @@ createApp({
     data() {
         return {
             generateMail: [],
+            
+            
         }
     },
 
     mounted() {
+
         //ciclo con il for per fare dieci volte la stessa operazione
         for (let i = 0; i < 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //promessa
+           axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //promessa
                 .then(response => {
-                    console.log(response);
-                    console.log(response.data.response);
-                    this.generateMail.push( response.data.response) 
+                    // console.log(response);
+                    // console.log(response.data.response);
+                    this.generateMail.push(response.data.response)
+                   
                 })
+
+            // .catch(error => {
+            // console.log(error);
+            // })
+
+
         }
     },
+
+
     methods: {
 
     }
