a79b8de4460f2106e53796c49d00c9600673449f
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-09-20 15:28:41 +0200

    generate one email

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,46 @@
-const { createApp } = Vue
 
+/* 
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        generateMail: "",
       }
-    }
+    },
+
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail') //promessa
+    .then(response =>{
+        console.log(response);
+        console.log(response.data.response);
+        this.generateMail = response.data.response
+        
+    })
+  },
+
+  methods: {
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+  }
+ 
+ 
+ 
+ 
+ 
+
+  
   }).mount('#app')
